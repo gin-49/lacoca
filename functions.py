6cadb67fe3b1bded62b1ae9a68986fb55395314b
gin <49gin49@gmail.com>
2025-11-26 01:54:55 -0400

    fix ik

--- functions.py
+++ functions.py
@@ -2,51 +2,33 @@ import math
 
 # Funcion para el calculo de la kinematica inversa del scara
 
-# TODO Implementar Forward Kinematics para Visualizacion, Transformar SVG a CSV y a Angulos, Generar trayectorias, Calculos de Dinamica torque velocidad,etc?, GUI en tkinter
+# TODO Implementar Forward Kinematics para Visualizacion, Transformar SVG a
+# CSV y a Angulos, Generar trayectorias, Calculos de Dinamica torque velocidad,etc?, GUI en tkinter
 
 
-def ik_scara(Xc, Yc, d, l1, l2, l3, l4):
-    # Ubicacion de los motores
-    m1 = -d / 2  # Izquierdo
-    m2 = d / 2  # Derecho
+def ik_scara(Xc, Yc, l1, l2, l5):
+    # Left arm
+    c = math.sqrt(Xc**2 + Yc**2)
+    if c > l1 + l2 or c < abs(l1 - l2):
+        return None  # Target unreachable
+    theta1 = math.atan2(Yc, Xc) + math.acos((l1**2 + c**2 - l2**2) / (2 * l1 * c))
 
-    try:
-        # Distancia punto a punto
-        pp1 = math.sqrt((m1 - Xc) ** 2 + (Yc) ** 2)
-        pp2 = math.sqrt((m2 - Xc) ** 2 + (Yc) ** 2)
+    # Right arm (base at x = l5)
+    e = math.sqrt((l5 - Xc) ** 2 + Yc**2)
+    if e > l1 + l2 or e < abs(l1 - l2):
+        return None  # Target unreachable
+    theta2 = math.atan2(Yc, Xc - l5) - math.acos((l1**2 + e**2 - l2**2) / (2 * l1 * e))
 
-        # Angulos internos del motor izquierdo
-        alpha1 = math.acos((l1**2 - l2**2 + pp1) / (2 * l1 * math.sqrt(pp1)))
-        beta1 = math.atan2(Yc, Xc)
-
-        # Angulos internos del motor derecho
-        alpha2 = math.acos((l4**2 - l3**2 + pp2) / (2 * l4 * math.sqrt(pp2)))
-        beta2 = math.atan2(Yc, Xc + d)
-
-        # Suma de alpha y beta ajustando para el angulo del paso a paso
-        theta1 = math.degrees(alpha1 + beta1) - 90
-        theta2 = math.degrees(alpha2 + beta2) - 90
-
-        if alpha1 and beta2 > 0:
-            return theta1, theta2
-        else:
-            return "Posición Inalcanzable"
-
-    # Manejo de Errores
-    except ValueError:
-        return None, None
+    return math.degrees(theta1), math.degrees(theta2)
 
 
 # TEST
+xc = 100
+yc = 105
+l1 = 100
+l2 = 150
+l5 = 106.249
 
-xc = 10
-yc = 10
-d = 106.249
-l1 = 10
-l2 = 15
-l3 = 15
-l4 = 10
-
-path = ik_scara(xc, yc, d, l1, l2, l3, l4)
-
-print(path)
+ik = ik_scara(xc, yc, l1, l2, l5)
+print(ik)
+print(xc, yc)
--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from stepper import Stepper
 import time
 
 s1 = Stepper(18, 19, steps_per_rev=3200, speed_sps=1500, timer_id=0)
+s2 = Stepper(16, 17, steps_per_rev=3200, speed_sps=1500, timer_id=1)
 
 while True:
-    s1.target_deg(-180)
-    time.sleep(4.0)
-    s1.target_deg(0)
-    time.sleep(4.0)
+    theta1 = float(input("theta1: "))
+    s1.target_deg(theta1)
+    print("Movement started")
